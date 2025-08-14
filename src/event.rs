bd9e19ff353b898247661d1b8c8d2ea106a86419
horus <h.horace0921@gmail.com>
2025-08-14 23:16:37 +0800

    chore: tmp cm

--- src/event.rs
+++ src/event.rs
@@ -1,1 +1,31 @@
+// Raw -> Indexer -> InstrumentKey (ExchangeId, InstrumentId), Subscription (ExchangeId, InstrumentId, ChannelKind) -> MarketEvent -> EngineEvent
+// Raw -> Indexer -> InstrumentKey (ExchangeId, InstrumentId), Subscription (ExchangeId, InstrumentId, ChannelKind) -> AccountEvent -> EngineEvent
+//
+// EngineEvent -> <Processor>
+// Clock -> match EngineEvent -> Strategy -> AuditTick
+//
+// Strategy -> (Opens, Cancels) -> RiskManager -> (OpensApproved, CancelsApproved, OpensRejected, CancelsRejected) -> Apprroved to InFlightRecord
+//
+// ExecutionTxs -> FnvIndexMap
+//
+// MarketEvent
+//  time_exchange
+//  time_local
+//  kind
+//  exchange?
+//  instrument?
+//
+// AccountEvent
+//  time_exchange
+//  time_local
+//  kind
+//  exchange?
+//
+// EngineEvent
+//  shutdown
+//  command
+//  feature_switch
+//  market_event
+//  account_event
 
+// MarketConstraintFuture
